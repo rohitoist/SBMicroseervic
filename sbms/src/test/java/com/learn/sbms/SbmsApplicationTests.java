efed7401ae67149e7467e546cbde3a90b93813d6
rohit <rohit.2.gupta@atos.net>
2023-05-05 00:45:34 +0100

    Signed-off-by: rohit <rohit.2.gupta@atos.net> spring boot microservice base project commit

---
+++ sbms/src/test/java/com/learn/sbms/SbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.sbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
